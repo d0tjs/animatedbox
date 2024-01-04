cbbc4812f1022e90ed57bd50b909d67d557da5d0
d0tjs <aappdotjs@gmail.com>
2024-01-04 20:28:01 +0600

    my animation with js

---
+++ app.js
@@ -0,0 +1,23 @@
+const links = document.getElementsByTagName("p");
+setInterval(() => {
+  for (let i = 0; i < links.length; i++) {
+    if (i % 2 == 0) {
+      links[i].style.width = "100%";
+      links[i].style.border = "5px dotted green";
+    } else {
+      links[i].style.width = "120px";
+      links[i].style.border = "5px dotted red";
+    }
+  }
+}, 1000);
+setInterval(() => {
+  for (let i = 0; i < links.length; i++) {
+    if (i % 2 == 0) {
+      links[i].style.width = "120px";
+      links[i].style.border = "2px solid red";
+    } else {
+      links[i].style.width = "100%";
+      links[i].style.border = "5px dotted green";
+    }
+  }
+}, 2000);
